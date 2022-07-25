424037826dd51b46fda32a8a1087209738389327
Irfan Ali <irfan.htc40@gmail.com>
2022-07-25 14:30:49 +0200

    setup database

---
+++ src/infrastructure/entities/product.entity.ts
@@ -0,0 +1,29 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from 'mongoose';
+
+export type ProductDocument = Product & Document;
+
+class Discount {
+  @Prop()
+  price: number;
+
+  @Prop()
+  quantity: number;
+}
+
+@Schema()
+export class Product {
+  @Prop({ required: true })
+  pid: string;
+
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  unitPrice: number;
+
+  @Prop()
+  discount: Discount;
+}
+
+export const ProductSchema = SchemaFactory.createForClass(Product);
