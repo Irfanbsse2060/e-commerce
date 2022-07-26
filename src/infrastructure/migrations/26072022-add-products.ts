3f9125030bcdd785ef364d97f74912dadf70fcca
Irfan Ali <irfan.htc40@gmail.com>
2022-07-26 14:27:59 +0200

    refactor migration

---
+++ src/infrastructure/migrations/26072022-add-products.ts
@@ -0,0 +1,14 @@
+import { getDb } from '../migrations-utils/db';
+import products from '../../mocks/products';
+
+export const up = async () => {
+  const db = await getDb();
+  const insertResult = await db.collection('products').insertMany(products);
+  console.log('Inserted documents =>', insertResult);
+};
+
+export const down = async () => {
+  const db = await getDb();
+  const deleteResult = await db.collection('products').deleteMany({pid: {$in:products.map(product => product.pid)}});
+  console.log('Removed documents =>', deleteResult);
+};
